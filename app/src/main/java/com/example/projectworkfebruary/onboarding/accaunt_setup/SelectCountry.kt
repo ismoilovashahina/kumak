dfb8d67ec7055c3f98fbc0d7f7e3630cbe0597e5
PC671 <ismoilovashahina@gmail.com>
2023-03-27 15:37:31 +0500

    +fontfamily

---
+++ app/src/main/java/com/example/projectworkfebruary/onboarding/accaunt_setup/SelectCountry.kt
@@ -0,0 +1,14 @@
+package com.example.projectworkfebruary.onboarding.accaunt_setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectworkfebruary.R
+
+class SelectCountry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_country)
+
+
+    }
+}
